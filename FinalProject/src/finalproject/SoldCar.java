7d761ab845010bc4874a73d78d28c99a789a9cad
Kirtis O <kkombb@mail.missouri.edu>
2017-12-05 12:04:50 -0600

    A separate class for cars in the sold table.

---
+++ FinalProject/src/finalproject/SoldCar.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author coast
+ */
+public class SoldCar {
+    
+    private String make;
+    private String model;
+    private float price;
+    private String name;
+    
+    public SoldCar(String make, String model, float price, String name){
+        this.make = make;
+        this.model = model;
+        this.price = price;
+        this.name = name;
+    }
+    
+    public void setMake(String make){
+        this.make = make;
+    }
+    
+    public String getMake(){
+        return this.make;
+    }
+    
+    public void setModel(String model){
+        this.model = model;
+    }
+    
+    public String getModel(){
+        return this.model;
+    }
+    
+    public void setPrice(float price){
+        this.price = price;
+    }
+    
+    public float getPrice(){
+        return this.price;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
